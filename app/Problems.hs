ce7dbf9d6527deb152535d975833de9a5c482ffb
Tanner Bruce <tanner@tanners.space>
2015-10-20 00:45:08 -0500

    Problems 1,2,3,4,5

--- app/Problems.hs
+++ app/Problems.hs
@@ -1,2 +1,15 @@
+problemOne :: [a] -> a
 problemOne = head . reverse
 
+problemTwo :: [a] -> a
+problemTwo = head . tail . reverse
+
+problemThree :: Int -> [b] -> b
+problemThree x y = head $ flip drop y (x-1)
+
+problemFour :: [a] -> Int
+problemFour = foldr (\_ x -> x+1) 0
+
+problemFive :: [a] -> [a]
+problemFive [] = []
+problemFive (x:xs) = problemFive xs ++ [x]
