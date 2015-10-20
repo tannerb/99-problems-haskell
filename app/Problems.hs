baa072506f0c350d9ce87cf49e991e576dee3668
Tanner Bruce <tanner@tanners.space>
2015-10-20 16:04:55 -0500

    Problems 12 14 15 16 17 18 19wip

--- app/Problems.hs
+++ app/Problems.hs
@@ -50,3 +50,58 @@ pack (x:xs) = (x : takeWhile (==x) xs) : pack (dropWhile (==x) xs)
 -- | problem 10. run length encode
 rle :: Eq a => [a] -> [(Int,a)]
 rle xs = [(length x, head x) | x <- pack xs]
+
+-- | problem 11. modified run length encode
+data RLPair a = Multiple Int a | Single a deriving (Show)
+
+mrle :: Eq a => [a] -> [RLPair a]
+mrle xs = [toRLPair x | x <- rle xs]
+  where toRLPair (1,y) = Single y
+        toRLPair (x,y) = Multiple x y
+
+-- | problem 12. decode mrle
+decodeMrle :: Eq a => [RLPair a] -> [a]
+decodeMrle [] = []
+decodeMrle (x:xs) = decodeRLPair x ++ decodeMrle xs
+  where decodeRLPair (Single x) = [x]
+        decodeRLPair (Multiple n x) = replicate n x
+
+-- | problem 13. 
+
+-- encodeDirect :: Eq a => [a] -> [RLPair a]
+-- encodeDirect (x:xs)
+
+-- | problem 14. Duplicate elements of a list
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x:x:dupli xs
+
+-- | problem 15. replicate the elements of list n times
+repli :: Int -> [a] -> [a]
+repli n [] = []
+repli n xs = concatMap (replicate n) xs
+
+-- | problem 16. drop every n'th element
+dropEvery :: Int -> [a] -> [a]
+dropEvery n [] = []
+dropEvery n xs 
+    | length xs < n = xs
+    | otherwise = (take (n-1) xs) ++ (dropEvery n $ drop n xs)
+
+-- | problem 17. split a list in two given the size of the first
+split :: [a] -> Int -> [[a]]
+split [] n = []
+split xs n = [(take n xs),(drop n xs)]
+
+-- | problem 18. slice a list with i and k
+slice :: [a] -> Int -> Int -> [a]
+slice [] _ _ = []
+slice xs i k =  drop (i-1) $ take k xs
+
+-- | problem 19. rotate a list n places to the left
+rotate :: [a] -> Int -> [a]
+rotate [] _ = []
+rotate [xs] _ = [xs]
+rotate xs 0 = xs
+rotate (x:xs) n = rotate ((last xs):(init xs) ++ [x]) (n-1)
